1b92f60e751c5d9aa2d4c27e7fc96e550379459e
Cronberry <55695062+cronberry@users.noreply.github.com>
2019-12-20 12:08:19 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 
 importScripts('https://www.gstatic.com/firebasejs/5.5.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.5.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/4.1.1/firebase.js');
 
 var firebaseConfig = {
     apiKey: "<FIREBASE-APIKEY",
